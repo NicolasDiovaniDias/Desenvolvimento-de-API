51ec34615b23cf17e96c6a3c3474d57e25ad27fb
Nicolas Diovani <nicolasdiovanioliveira@gmail.com>
2025-03-18 11:49:28 -0300

    18.03.25 11:49

---
+++ aula 02/script.js
@@ -0,0 +1,8 @@
+$("#minha_div").html('Ola <i>Pessoal</i>')
+$("#minha_div").hide()
+    $("#botao").click(function(){
+        $("#minha_div").toggle()
+    })
+$("#botao").on("click","button", function(){
+    
+})
